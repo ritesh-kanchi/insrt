414e08b924ca8d48f8a2a958b9fa2cfc1f4c9f63
Ritesh Kanchi <ritesh.kanchi@hotmail.com>
2021-05-13 22:55:30 -0500

    Initial Commit

---
+++ insrt/insrtApp.swift
@@ -0,0 +1,17 @@
+//
+//  insrtApp.swift
+//  insrt
+//
+//  Created by Ritesh Kanchi on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct insrtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
